c664bdd5100ce4a2f0cf235e6d0db112548aa9dc
Ali Hosseinzad <hooseizad.ali.n900@gmail.com>
2020-12-09 17:20:44 +0330

    add new canvas file

--- src/components/main layout/Canvas.js
+++ src/components/main layout/Canvas.js
@@ -1,43 +1,8 @@
-// import React, { Component } from "react";
-// import { Canvas } from "react-canvas-js";
-
-// const sample1 = {
-//   maxParticles: 70,
-//   colors: [
-//     "#00FFFF",
-//     "#9932CC",
-//     "#487EEF",
-//     "#FF1493",
-//     "#fc5c65",
-//     "#fed330",
-//     "#7FFF00",
-//   ],
-//   shapes: ["circle", "square"],
-//   size: 9,
-//   minSpeed: 0.05,
-//   maxSpeed: 0.2,
-//   alpha: 0.7,
-//   backgroundColor: "#1E1F29",
-// };
-
-// export default class App extends Component {
-
-//   render() {
-//     return (
-//       <>
-//         <div id="scrollableDiv" style={{ height: 300, overflow: "auto" }}>
-//           <InfiniteScroll
-//             dataLength={this.state.items.length}
-//             next={this.fetchMoreData}
-//             hasMore={true}
-//             scrollableTarget="scrollableDiv"
-//           >
-//             {this.state.items.map((i, index) => (
-//               <Canvas options={sample1} key={index} />
-//             ))}
-//           </InfiniteScroll>
-//         </div>
-//       </>
-//     );
-//   }
-// }
+import React, { Component } from "react";
+import Particles from "react-particles-js";
+export default class App extends Component {
+  render() {
+    console.log(this.props.param);
+    return <Particles params={this.props.param} />;
+  }
+}
